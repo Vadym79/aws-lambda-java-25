5ff6fe2ae1bc91f4ae8ab330bf70e0a69c33243f
Vadym Kazulkin <v.kazulkin@gmail.com>
2026-02-15 11:21:39 +0100

    frist attempt for AWS Lambda with Aurora DSQL and GraalVM with Java 25

---
+++ aws-lambda-java-25-aurora-dsql-as-graalvm-native-image/src/main/java/software/amazonaws/example/product/dao/ProductDao.java
@@ -0,0 +1,81 @@
+package software.amazonaws.example.product.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+
+import software.amazonaws.example.product.entity.Product;
+
+public class ProductDao {
+		
+	@SuppressWarnings("unused")
+	private static final DsqlDataSourceConfig dsqlDataSourceConfig=new DsqlDataSourceConfig();
+	
+	/**
+	 * create a product and return its id
+	 * 
+	 * @param product product
+	 * @return product id
+	 */
+	public int createProduct(Product product) throws Exception {
+		try (Connection con = getConnection()) {
+			try (PreparedStatement pst = this.createProductPreparedStatement(con, product)) {
+				pst.executeUpdate();
+			} catch (SQLException ex) {
+				con.rollback();
+				throw ex;
+			}
+		}
+      return product.id();
+	}
+	
+
+
+	/**
+	 * returns product by its id 
+	 * 
+	 * @param id -product id
+	 * @return 
+	 * @throws Exception
+	 */
+	public Optional<Product> getProductById(int id) throws Exception {
+		try (Connection con = getConnection();
+				PreparedStatement pst = this.getProductByIdPreparedStatement(con, id);
+				ResultSet rs = pst.executeQuery()) {
+			if (rs.next()) {
+				var name = rs.getString("name");
+				int price = rs.getInt("price");
+				var product = new Product(id, name, price);
+				return Optional.of(product);
+			} else {
+				return Optional.empty();
+			}
+		}
+
+	}
+	
+	
+	private PreparedStatement createProductPreparedStatement(Connection con, Product product) throws SQLException {
+		var pst = con.prepareStatement("INSERT INTO products VALUES (?, ?, ?) ");		
+		pst.setInt(1, product.id());
+		pst.setString(2, product.name());
+		pst.setInt(3, product.price());
+		return pst;
+	}
+
+
+	private PreparedStatement getProductByIdPreparedStatement(Connection con, int id) throws SQLException {
+		PreparedStatement pst = con.prepareStatement("SELECT * FROM products WHERE id = ?");
+		pst.setInt(1, id);
+		return pst;
+	}
+	
+	
+	private static final Connection getConnection() throws SQLException {
+		 return DsqlDataSourceConfig.getPooledConnection();
+		//return DsqlDataSourceConfig.getJDBCConnection();
+	}
+}
